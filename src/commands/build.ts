3061b7599e1a588f9107acb71667707db58a9427
Juniper Jorgensen <juniper@leprechaun.dev>
2026-02-27 09:38:41 -0800

    better error handling

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,6 +1,10 @@
 import { TartanArgs } from "../program.js";
 import { loadConfig, TartanConfig } from "../util/config.js";
-import { useOutput, TestableCommandModule } from "../util/outputs.js";
+import {
+    useOutput,
+    TestableCommandModule,
+    formatError,
+} from "../util/outputs.js";
 import { inspect } from "node:util";
 import {
     ContextTreeNode,
@@ -21,29 +25,32 @@ const module: TestableCommandModule<{}, TartanArgs> = {
     handler: async (args, output) => {
         const { logger, stdout } = useOutput(output);
         const config: TartanConfig = await loadConfig(args, logger);
-
         logger.debug(`using config\n${inspect(config, { colors: true })}`);
-        const baseLogger = createLogger({
-            transports: [new NullTransport()], // supress core logs by default
-        });
+        try {
+            const baseLogger = createLogger({
+                transports: [new NullTransport()], // supress core logs by default
+            });
 
-        const node: ContextTreeNode = await loadContextTreeNode({
-            directory: config.sourceDirectory,
-            rootContext: config.rootContext,
-            baseLogger,
-        });
-        const processed: ProcessedNode = await processNode({
-            node,
-            rootContext: config.rootContext,
-            sourceDirectory: config.sourceDirectory,
-            baseLogger,
-        });
-        const resolved: ResolvedNode = resolveNode(processed);
-        const finalized: ResolvedNode = await finalizeNode({
-            node: resolved,
-            sourceDirectory: config.sourceDirectory,
-        });
-        await outputNode(finalized, config.outputDirectory);
+            const node: ContextTreeNode = await loadContextTreeNode({
+                directory: config.sourceDirectory,
+                rootContext: config.rootContext,
+                baseLogger,
+            });
+            const processed: ProcessedNode = await processNode({
+                node,
+                rootContext: config.rootContext,
+                sourceDirectory: config.sourceDirectory,
+                baseLogger,
+            });
+            const resolved: ResolvedNode = resolveNode(processed);
+            const finalized: ResolvedNode = await finalizeNode({
+                node: resolved,
+                sourceDirectory: config.sourceDirectory,
+            });
+            await outputNode(finalized, config.outputDirectory);
+        } catch (err: any) {
+            logger.error(formatError(err));
+        }
     },
 };
 
